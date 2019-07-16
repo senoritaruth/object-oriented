87c7c13c1adf1f95075b4975cff4a2627dd98ede
Ruth Cisneros <senoritaruth@gmail.com>
2019-07-15 23:50:23 -0600

    wrote and documented all state variables in the class

---
+++ php/Classes/Foo.php
@@ -0,0 +1,13 @@
+class Foo
+{
+
+	/**
+ 	* @var string $authorId Primary key
+ 	* @var string|null $authorAvatarUrl URL for profile picture
+ 	* @var string $authorActivationToken Activation token
+ 	* @var string $authorEmail Author email address
+ 	* @var string $authorHash Author hash
+ 	* @var string $authorUsername Author user name
+	**/
+
+}

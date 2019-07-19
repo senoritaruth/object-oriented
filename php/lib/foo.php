985ba52c796d9a25c66db7aaa5fe40f1bf5b16b0
Ruth Cisneros <senoritaruth@gmail.com>
2019-07-19 02:36:43 -0600

    changed slashes again

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -1,7 +1,7 @@
 <?php
 namespace rcisneros\ObjectOriented;
 
-require_once (dirname(__DIR__, 2) ."\php\Classes\Foo.php");
+require_once (dirname(__DIR__, 2) ."/php/Classes/Foo.php");
 
 /** @var author $foo instantiation test **/
 $foo = new author("111111111111111111111111111111111111", "", "lwefk32", "RitaLLot@gsnail.com", "alkdjfiaikjfp", "Rita Lot");

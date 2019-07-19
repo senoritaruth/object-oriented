5d6e563bcbbb11a2f31eaffecbfb00d5e476887a
Ruth Cisneros <senoritaruth@gmail.com>
2019-07-19 01:04:07 -0600

    corrected constructor call

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -3,6 +3,6 @@ namespace rcisneros\ObjectOriented;
 
 require once ("../Classes/Foo.php");
 
-$foo = new author("abcd");
+$foo = new author("abcd", null, "lwefk32", "RitaLLot@gsnail.com", "alkdjfiaikjfp", "Rita Lot");
 
 var_dump($foo);

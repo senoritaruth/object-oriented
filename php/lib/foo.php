3ddb64877e063050646dfc346aea13424f244198
Ruth Cisneros <senoritaruth@gmail.com>
2019-07-19 11:35:02 -0600

    changed uuid, plugged new one in with tacks

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -4,6 +4,6 @@ namespace rcisneros\ObjectOriented;
 require_once (dirname(__DIR__, 2) ."/php/Classes/Foo.php");
 
 /** @var author $foo instantiation test **/
-$foo = new author("111111111111111111111111111111111111", "", "lwefk32", "RitaLLot@gsnail.com", "alkdjfiaikjfp", "Rita Lot");
+$foo = new author("I48@$0$Cs5#H", "", "lwefk32", "RitaLLot@gsnail.com", "alkdjfiaikjfp", "Rita Lot");
 
 var_dump($foo);
